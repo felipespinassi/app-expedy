4468c31526ad624aa83d4a3891037799b66b2e81
felipespinassi <felipe_spinassi@hotmail.com>
2022-08-16 20:20:31 -0300

    estilizando screen login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,32 +1,49 @@
 import { Button, TextInput } from "react-native-paper";
-import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Alert, View } from "react-native";
+import { styles } from "./styles";
 
 export const Login = () => (
-  <View>
-    <Image style={styles.image} source={require("../../../assets/icon.png")} />
-    <View style={styles.container}>
+  <View style={styles.container}>
+    <View style={styles.containerInput}>
       <TextInput
+        underlineColor="red"
+        mode="flat"
+        selectionColor="red"
+        activeUnderlineColor="red"
+        style={styles.input}
         keyboardType="numeric"
         placeholder="Código"
         placeholderTextColor="#6b6b6b"
       />
-      <TextInput placeholder="Login" placeholderTextColor="#6b6b6b" />
-      <TextInput placeholder="Senha" placeholderTextColor="#6b6b6b" />
+      <TextInput
+        mode="flat"
+        underlineColor="red"
+        selectionColor="red"
+        activeUnderlineColor="red"
+        style={styles.input}
+        placeholder="Login"
+        placeholderTextColor="#6b6b6b"
+      />
+      <TextInput
+        secureTextEntry={true}
+        mode="flat"
+        underlineColor="red"
+        selectionColor="red"
+        activeUnderlineColor="red"
+        style={styles.input}
+        placeholder="Senha"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
-    <Button
-      buttonColor="red"
-      mode="contained"
-      onPress={() => Alert.alert("", "Logou")}
-    >
-      Login
-    </Button>
+    <View style={styles.containerButton}>
+      <Button
+        style={styles.button}
+        buttonColor="red"
+        mode="contained"
+        onPress={() => console.log("Logou")}
+      >
+        Login
+      </Button>
+    </View>
   </View>
 );
-
-const styles = StyleSheet.create({
-  container: {},
-  image: {
-    width: "100%",
-    height: 150,
-  },
-});
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center" },
+  containerInput: {
+    alignItems: "center",
+  },
+  input: {
+    marginTop: 20,
+    backgroundColor: "#eee",
+    width: "90%",
+  },
+  containerButton: {
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 25,
+    width: "60%",
+  },
+});
