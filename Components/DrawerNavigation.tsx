d3a4ace377248fd9c2433c1afc62749dd7749da9
JonneBR <felipe_spinassi@hotmail.com>
2022-08-18 17:23:04 -0300

    adicionando tela de lista de separação

--- Components/DrawerNavigation.tsx
+++ Components/DrawerNavigation.tsx
@@ -1,13 +1,15 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { Dashboard } from "../src/screens/Dashboard";
+import { ListaSeparacao } from "../src/screens/ListaSeparacao";
 
 const Drawer = createDrawerNavigator();
 
 export function DrawerNavigation() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Dashboard} />
+      <Drawer.Screen name="Bem vindo" component={Dashboard} />
+      <Drawer.Screen name="Lista de separação" component={ListaSeparacao} />
     </Drawer.Navigator>
   );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import { Text } from "react-native";
 export function Dashboard() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Dashboard</Text>
+      <Text>Bem vindo a Expedy, sua expedição inteligente</Text>
     </View>
   );
 }
