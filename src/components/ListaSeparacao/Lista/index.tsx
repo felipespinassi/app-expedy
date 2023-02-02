1274718780df655559fd738f9dccfcc55edca0cb
felipespinassi <felipe_spinassi@hotmail.com>
2023-02-01 23:16:26 -0300

    adicionando TouchableOpacity

--- src/components/ListaSeparacao/Lista/index.tsx
+++ src/components/ListaSeparacao/Lista/index.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Divider, Progress, Text, View } from "native-base";
+import { TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 type ItemProps = {
@@ -10,30 +11,32 @@ type ItemProps = {
 export function Lista({ item }: ItemProps) {
   return (
     <>
-      <View style={styles.container}>
-        <View>
-          <Text>ID</Text>
-          <Text>{item.idERP_Lista}</Text>
+      <TouchableOpacity onPress={() => console.log(item.idERP_Lista)}>
+        <View style={styles.container}>
+          <View>
+            <Text>ID</Text>
+            <Text>{item.idERP_Lista}</Text>
+          </View>
+          <View>
+            <Avatar bg="green.500">T</Avatar>
+          </View>
+          <View width={"50%"}>
+            <Text style={{ marginBottom: 5 }}>Tuccihome</Text>
+            <Progress
+              bg="#fff"
+              value={100}
+              mx="1"
+              _filledTrack={{
+                bg: "lime.500",
+              }}
+            />
+          </View>
         </View>
-        <View>
-          <Avatar bg="green.500">T</Avatar>
-        </View>
-        <View width={"50%"}>
-          <Text style={{ marginBottom: 5 }}>Tuccihome</Text>
-          <Progress
-            bg="#fff"
-            value={50}
-            mx="1"
-            _filledTrack={{
-              bg: "lime.500",
-            }}
-          />
-        </View>
-      </View>
 
-      <View alignItems={"center"}>
-        <Divider bg="light.400" thickness={"1"} w={"90%"} />
-      </View>
+        <View alignItems={"center"}>
+          <Divider bg="light.400" thickness={"1"} w={"90%"} />
+        </View>
+      </TouchableOpacity>
     </>
   );
 }
