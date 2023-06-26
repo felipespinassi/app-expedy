3c72476d7ee204875f73e6bc66c630e2f5fc045f
Felipe <felipe_spinassi@hotmail.com>
2023-06-25 22:39:16 -0300

    add get pedidos

--- src/screens/Pedidos/Pedidos.tsx
+++ src/screens/Pedidos/Pedidos.tsx
@@ -1,18 +1,35 @@
-import { Text } from "react-native";
-import React, { Component } from "react";
-import { View } from "native-base";
+import { Text, TouchableOpacity } from "react-native";
+import React, { Component, useEffect, useState } from "react";
+import { FlatList, View } from "native-base";
+import { getService } from "../../services/getService";
 
 export default function Pedidos() {
-  {
-    return (
-      <>
-        <View
-          backgroundColor={"light.200"}
-          style={{ height: 100, marginBottom: 5 }}
-        >
-          <Text>felipinho</Text>
-        </View>
-      </>
-    );
+  const [pedidos, setPedidos] = useState([]);
+  async function fetchData() {
+    try {
+      const response: any = await getService("front/orders/simples");
+
+      setPedidos(response.data.pedidos);
+    } catch (error) {}
   }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <FlatList
+      data={pedidos}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => console.log(item)}>
+          <View
+            backgroundColor={"light.200"}
+            style={{ height: 100, marginBottom: 5 }}
+          >
+            <Text>felipinho</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    ></FlatList>
+  );
 }
