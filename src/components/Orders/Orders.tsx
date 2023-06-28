6be2f14ece152ba6bce9197178c91c4eef33613f
Felipe Augusto <77703754+felipespinassi@users.noreply.github.com>
2023-06-28 17:21:26 -0300

    add filter icon

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -16,74 +16,91 @@ export default function Orders({ navigation }: any) {
   );
   return (
     <View style={{ alignItems: "center" }}>
+      <TouchableOpacity
+        style={{
+          justifyContent: "flex-start",
+          width: "90%",
+          flexDirection: "row",
+          paddingVertical: 10,
+        }}
+      >
+        <Text>Filtros</Text>
+        <AntDesign name="filter" size={22} />
+      </TouchableOpacity>
       {!isLoading ? (
-        <FlatList
-          refreshing={isLoading}
-          onRefresh={() => refetch()}
-          showsVerticalScrollIndicator={false}
-          style={{ width: "90%" }}
-          data={data?.data?.pedidos}
-          renderItem={({ item }: any) => (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("PedidoId", item.id);
-              }}
-            >
-              <View
-                style={{
-                  height: 120,
-                  marginBottom: 5,
-                  padding: 5,
-                  justifyContent: "space-between",
-                  flexDirection: "row",
+        <View style={{ alignItems: "center" }}>
+          <FlatList
+            refreshing={isLoading}
+            onRefresh={() => refetch()}
+            showsVerticalScrollIndicator={false}
+            style={{ width: "90%" }}
+            data={data?.data?.pedidos}
+            renderItem={({ item }: any) => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("PedidoId", item.id);
                 }}
               >
                 <View
                   style={{
-                    justifyContent: "space-around",
-                    width: 210,
+                    height: 120,
+                    marginBottom: 5,
+                    padding: 5,
+                    justifyContent: "space-between",
+                    flexDirection: "row",
                   }}
                 >
-                  <Heading fontWeight={500} size="xs">
-                    {item.integracao.name}
-                  </Heading>
-                  <Text>
-                    Data:
-                    {moment(item.date).utc(true).format("DD/MM/YYYY")}:
-                    {moment(item.date).utc(true).format("HH:mm")}
-                  </Text>
+                  <View
+                    style={{
+                      justifyContent: "space-around",
+                      width: 210,
+                    }}
+                  >
+                    <Heading fontWeight={500} size="xs">
+                      {item.integracao.name}
+                    </Heading>
+                    <Text>
+                      Data:
+                      {moment(item.date).utc(true).format("DD/MM/YYYY")}:
+                      {moment(item.date).utc(true).format("HH:mm")}
+                    </Text>
 
-                  <Text>{item.Customer.name}</Text>
-                </View>
+                    <Text>{item.Customer.name}</Text>
+                  </View>
 
-                <View
-                  style={{
-                    justifyContent: "space-around",
-                    alignItems: "center",
-                    width: 150,
-                  }}
-                >
-                  {statusHub[item.status_hub]?.box}
-                  <View style={{ flexDirection: "row" }}>
-                    {item.erroNota && (
-                      <AntDesign color={"red"} name="exclefile1" size={22} />
-                    )}
-                    {item.erroEtiqueta && (
-                      <AntDesign color={"red"} name="filetext1" size={22} />
-                    )}
-                    {item.statusNota && !item.erroNota && (
-                      <AntDesign color={"green"} name="exclefile1" size={22} />
-                    )}
-                    {item.etiqueta && !item.erroEtiqueta && (
-                      <AntDesign color={"green"} name="filetext1" size={22} />
-                    )}
+                  <View
+                    style={{
+                      justifyContent: "space-around",
+                      alignItems: "center",
+                      width: 150,
+                    }}
+                  >
+                    {statusHub[item.status_hub]?.box}
+                    <View style={{ flexDirection: "row" }}>
+                      {item.erroNota && (
+                        <AntDesign color={"red"} name="exclefile1" size={22} />
+                      )}
+                      {item.erroEtiqueta && (
+                        <AntDesign color={"red"} name="filetext1" size={22} />
+                      )}
+                      {item.statusNota && !item.erroNota && (
+                        <AntDesign
+                          color={"green"}
+                          name="exclefile1"
+                          size={22}
+                        />
+                      )}
+                      {item.etiqueta && !item.erroEtiqueta && (
+                        <AntDesign color={"green"} name="filetext1" size={22} />
+                      )}
+                    </View>
                   </View>
                 </View>
-              </View>
-              <Divider />
-            </TouchableOpacity>
-          )}
-        />
+                <Divider />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       ) : (
         <OrderSkelleton />
       )}
