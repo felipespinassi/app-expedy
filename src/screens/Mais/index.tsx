409978e63ef665a177f55604ed26b89747fbf61f
Felipe <felipe_spinassi@hotmail.com>
2023-10-05 00:57:21 -0300

    style screen Mais

--- src/screens/Mais/index.tsx
+++ src/screens/Mais/index.tsx
@@ -8,7 +8,7 @@ import {
 
 import React, { useState, useEffect } from "react";
 import ArrowBack from "../../components/ArrowBack/ArrowBack";
-import { Avatar, Heading } from "native-base";
+import { Avatar, Box, Heading } from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { COMPANY_NAME } from "../../storage/storageConfig";
@@ -78,34 +78,91 @@ export default function Mais({ navigation }: any) {
       <ScrollView style={{ paddingHorizontal: 15 }}>
         <TouchableOpacity
           onPress={() => navigation.navigate("Home")}
-          style={{ flexDirection: "row", padding: 10 }}
+          style={{ flexDirection: "row", padding: 5 }}
         >
-          <Ionicons name="home-outline" size={20} />
-          <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
-            Início
-          </Heading>
+          <Box
+            height={16}
+            w={"100%"}
+            rounded="lg"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _light={{
+              backgroundColor: "gray.50",
+            }}
+            px={3}
+            style={{ justifyContent: "center" }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons name="home-outline" size={20} />
+              <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
+                Início
+              </Heading>
+            </View>
+          </Box>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => navigation.navigate("Pedidos")}
-          style={{ flexDirection: "row", padding: 10 }}
+          style={{ flexDirection: "row", padding: 5 }}
         >
-          <Ionicons name="cart-outline" size={20} />
+          <Box
+            height={16}
+            w={"100%"}
+            rounded="lg"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _light={{
+              backgroundColor: "gray.50",
+            }}
+            px={3}
+            style={{ justifyContent: "center" }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons name="cart-outline" size={20} />
 
-          <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
-            Pedidos
-          </Heading>
+              <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
+                Pedidos
+              </Heading>
+            </View>
+          </Box>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => navigation.navigate("Expedicao")}
-          style={{ flexDirection: "row", padding: 10 }}
+          style={{ flexDirection: "row", padding: 5 }}
         >
-          <Ionicons name="cube-outline" size={20} />
+          <Box
+            height={16}
+            w={"100%"}
+            rounded="lg"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _light={{
+              backgroundColor: "gray.50",
+            }}
+            px={3}
+            style={{ justifyContent: "center" }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons name="cube-outline" size={20} />
 
-          <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
-            Expedição
-          </Heading>
+              <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
+                Expedição
+              </Heading>
+            </View>
+          </Box>
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -114,13 +171,32 @@ export default function Mais({ navigation }: any) {
               removeCompanyName(),
               navigation.navigate("Login");
           }}
-          style={{ flexDirection: "row", padding: 10 }}
+          style={{ flexDirection: "row", padding: 5 }}
         >
-          <Ionicons name="log-out-outline" size={20} />
+          <Box
+            height={16}
+            w={"100%"}
+            rounded="lg"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _light={{
+              backgroundColor: "gray.50",
+            }}
+            px={3}
+            style={{ justifyContent: "center" }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons name="log-out-outline" size={20} />
 
-          <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
-            Sair
-          </Heading>
+              <Heading fontWeight={400} size={"md"} style={{ paddingLeft: 10 }}>
+                Sair
+              </Heading>
+            </View>
+          </Box>
         </TouchableOpacity>
       </ScrollView>
     </>
