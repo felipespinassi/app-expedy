36fcef1e7901a17b6942340e79957d9c5db3d986
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-01-28 22:26:30 -0300

    feat: add get picking list in expedicao rapida

--- src/screens/ExpedicaoRapida/ArquivoGerado/index.tsx
+++ src/screens/ExpedicaoRapida/ArquivoGerado/index.tsx
@@ -1,27 +1,36 @@
-import { View, Text, Alert, TouchableOpacity } from 'react-native'
+import { View, Text, Alert } from 'react-native'
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 import React from 'react'
 import ArrowBack from '../../../components/ArrowBack/ArrowBack'
-import { Button, Heading } from 'native-base'
+import { Button, Center, Heading, ScrollView, VStack } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { getService } from '../../../services/getService'
-import AntDesign from "react-native-vector-icons/AntDesign";
+import { useQuery } from 'react-query';
+
 
 
 export default function ArquivoGerado(props: any) {
-
-    const item = props.route.params.id
+    const item = props.route.params._id
     const navigation: any = useNavigation()
+    const { data: response, isLoading, refetch, isFetching }: any = useQuery(
+        "OrderComplete",
+        async () => await getService(`orders/file/picking/${item}`, {})
+    );
+
+
+
+
+
 
     async function handlePrintFile(item: any) {
         const response: any = await getService(
-            `orders/file/print?id=${item.id}&usuario=Expedy`
+            `orders/file/print?id=${item}&usuario=Expedy`
         );
         if (response.status === 200) {
-            console.log(response);
 
             return Alert.alert("Imprimindo");
         } else {
-            console.log(response);
 
             return Alert.alert("Falha ao imprimir");
         }
@@ -45,14 +54,52 @@ export default function ArquivoGerado(props: any) {
 
             </View>
 
-            <TouchableOpacity
-                onPress={() => handlePrintFile(item)}
-                style={{ paddingTop: 5, display:'flex', flexDirection:'row', justifyContent:'space-around' }}
-            >
-                
-                <Button>Imprimir Arquivo</Button>
-                <Button>Ver Lista de Picking</Button>
-            </TouchableOpacity>
+            <ScrollView >
+                {response?.data.produtos.map((produto: any, index: any) => {
+                    return (
+                        <VStack>
+                            <Center
+                                shadow={1}
+                                rounded={"md"}
+                                bg={"light.50"}
+                                style={{
+                                    height: 120,
+                                    marginBottom: 10,
+                                    justifyContent: "space-between",
+                                    flexDirection: "row",
+                                    paddingHorizontal: 5,
+                                }}
+                            >
+
+                                <View key={index} style={{ height: 80, flexDirection: 'row', gap: 10, flexWrap: 'wrap', padding: 10 }}>
+
+                                    <Text >{produto.reference}</Text>
+                                    <Text >{produto.database_name}</Text>
+                                </View>
+                            </Center>
+                        </VStack>
+
+
+                    )
+                })}
+
+
+            </ScrollView>
+
+            <View style={{ paddingTop: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+                <TouchableOpacity
+                    onPress={() => handlePrintFile(item)}
+
+                >
+
+                    <Button style={{ backgroundColor: '#002851' }}>Imprimir Arquivo</Button>
+                </TouchableOpacity>
+
+
+
+            </View>
+
+
         </>
     )
 }
