071feb4175251047ab494aa756fc4a1a2bfe4c3a
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-02-01 22:07:27 -0300

    feat: add tamagui

--- App.tsx
+++ App.tsx
@@ -5,29 +5,41 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Routes } from "./src/Routes";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { TamaguiProvider, createTamagui } from 'tamagui'
+import { useColorScheme } from "react-native";
+import config from "./tamagui.config";
+import { useFonts } from 'expo-font'
 
 const queryClient = new QueryClient();
-const Stack = createNativeStackNavigator();
 
-const reactUpdates = async () => {
-  Updates.addListener((event) => {
-    if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
-      Updates.reloadAsync();
-    }
-  });
-};
+
+
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
   useEffect(() => {
-    reactUpdates();
-  }, []);
+    if (loaded) {
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null;
+  }
+
+
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <QueryClientProvider client={queryClient}>
         <NativeBaseProvider>
-          <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
-          <Routes />
+          <TamaguiProvider config={config} >
+            <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
+            <Routes />
+          </TamaguiProvider>
         </NativeBaseProvider>
       </QueryClientProvider>
     </GestureHandlerRootView>
