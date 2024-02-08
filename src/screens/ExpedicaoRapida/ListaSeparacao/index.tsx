8d59d54fb3ecd01178fed997fe050bd62aeb4baf
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-02-08 07:55:59 -0300

    feat: add dialog

--- src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
+++ src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
@@ -1,14 +1,16 @@
 import { View, Text, Alert } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-
-import React from "react";
+import { X } from '@tamagui/lucide-icons'
+import React, { useState } from "react";
 import ArrowBack from "../../../components/ArrowBack/ArrowBack";
 import { Button, Center, Heading, ScrollView, Spinner, VStack } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import { getService } from "../../../services/getService";
 import { useQuery } from "react-query";
+import { Adapt, Dialog, Fieldset, Input, Label, Paragraph, Sheet, TooltipSimple, Unspaced, XStack } from "tamagui";
 
 export default function ListaSeparacao(props: any) {
+  const [openModal,setOpenModal] = useState(false)
   const item = props.route.params;
   const navigation: any = useNavigation();
   const {
@@ -37,6 +39,95 @@ export default function ListaSeparacao(props: any) {
           Lista de Picking
         </Heading>
       </View>
+
+
+      <Dialog modal open={openModal} >
+      
+
+      <Adapt when="sm" platform="touch">
+        <Sheet animation="quick" zIndex={200000} modal dismissOnSnapToBottom>
+          <Sheet.Frame padding="$4" gap="$4">
+            <Adapt.Contents />
+          </Sheet.Frame>
+          <Sheet.Overlay
+            animation="lazy"
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+        </Sheet>
+      </Adapt>
+
+      <Dialog.Portal>
+        <Dialog.Overlay
+          key="overlay"
+          animation="slow"
+          opacity={0.5}
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+
+        <Dialog.Content
+          bordered
+          elevate
+          key="content"
+          animateOnly={['transform', 'opacity']}
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          gap="$4"
+        >
+          <Dialog.Title>Selecionar Quantidade</Dialog.Title>
+          <Dialog.Description>
+            Make changes to your profile here. Click save when you're done.
+          </Dialog.Description>
+          <Fieldset gap="$4" horizontal>
+            <Label width={160} justifyContent="flex-end" htmlFor="name">
+              Name
+            </Label>
+            <Input flex={1} id="name" defaultValue="Nate Wienert" />
+          </Fieldset>
+          <Fieldset gap="$4" horizontal>
+            <Label width={160} justifyContent="flex-end" htmlFor="username">
+              <TooltipSimple label="Pick your favorite" placement="bottom-start">
+                <Paragraph>Food</Paragraph>
+              </TooltipSimple>
+            </Label>
+          </Fieldset>
+
+          <XStack alignSelf="flex-end" gap="$4">
+
+            <Dialog.Close displayWhenAdapted asChild>
+              <Button onPress={() => setOpenModal(false)} aria-label="Close">
+                Save changes
+              </Button>
+            </Dialog.Close>
+          </XStack>
+
+          <Unspaced>
+          <Dialog.Close asChild>
+              <Button
+                position="absolute"
+                top="$3"
+                right="$3"
+                size="$2"
+                circular
+                icon={X}
+              />
+            </Dialog.Close>
+          </Unspaced>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+
+ 
+
 
       {isFetching ? (
         <Spinner />
@@ -73,12 +164,12 @@ export default function ListaSeparacao(props: any) {
                   <View
                     style={{ flexDirection: "row", justifyContent: "space-around", width: "100%" }}
                   >
-                    {/* <Button style={{ backgroundColor: "#002851" }} size={"xs"}>
+                     <Button style={{ backgroundColor: "#002851" }} size={"xs"}>
                       Confirmar todos
                     </Button>
-                    <Button style={{ backgroundColor: "#002851" }} size={"xs"}>
+                    <Button onPress={()=> setOpenModal(true)} style={{ backgroundColor: "#002851" }} size={"xs"}>
                       Selecionar quantidade
-                    </Button> */}
+                    </Button> 
                   </View>
                 </Center>
               </VStack>
