cc5113354bbff561b378e2651ce4c4cac4369083
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-02-08 17:23:09 -0300

    feat: add styles

--- src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
+++ src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
@@ -1,6 +1,6 @@
-import { View, Text, Alert } from "react-native";
+import { View, Alert, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 import { X } from "@tamagui/lucide-icons";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ArrowBack from "../../../components/ArrowBack/ArrowBack";
 import { useNavigation } from "@react-navigation/native";
 import { getService } from "../../../services/getService";
@@ -10,23 +10,22 @@ import {
   Button,
   Dialog,
   Fieldset,
-  H4,
   H5,
   Heading,
   Input,
   Label,
-  Paragraph,
   ScrollView,
   Sheet,
-  TooltipSimple,
+  Text,
   Unspaced,
   XStack,
 } from "tamagui";
 import { Center, Spinner, VStack } from "native-base";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function ListaSeparacao(props: any) {
   const [openModal, setOpenModal] = useState(false);
+  const produtoRef = useRef({} as any)
+
   const item = props.route.params;
   const navigation: any = useNavigation();
   const {
@@ -58,113 +57,120 @@ export default function ListaSeparacao(props: any) {
         <Spinner />
       ) : (
         <ScrollView style={{ paddingTop: 5 }}>
+
           {response?.data.produtos.map((produto: any, index: any) => {
+
             return (
-              <VStack key={index} style={{ paddingHorizontal: 5 }}>
-                <Center
-                  shadow={1}
-                  rounded={"md"}
-                  bg={"light.50"}
-                  style={{
-                    marginBottom: 10,
-                    paddingBottom: 10,
-                  }}
-                >
-                  <View
-                    key={index}
+              <TouchableOpacity onPress={() => { setOpenModal(true), produtoRef.current = produto }}>
+
+                <VStack key={index} style={{ paddingHorizontal: 5 }}>
+                  <Center
+                    shadow={1}
+                    rounded={"md"}
+                    bg={"light.50"}
                     style={{
-                      height: 80,
-                      flexDirection: "row",
-                      flexWrap: "wrap",
-                      padding: 10,
-                      justifyContent: "space-around",
-                      alignItems: "center",
-                      width: "100%",
+                      marginBottom: 10,
+                      paddingBottom: 10,
                     }}
                   >
-                    <Text>{produto.reference}</Text>
-                    <Text style={{ width: "40%" }}>{produto.database_name}</Text>
-                    <Text>{produto.quantity}</Text>
-                  </View>
-                  <View
-                    style={{ flexDirection: "row", justifyContent: "space-around", width: "100%" }}
-                  >
-                    <Button>Confirmar todos</Button>
-                    <Button onPress={() => setOpenModal(true)}> Selecionar quantidade</Button>
-                  </View>
-                </Center>
-              </VStack>
+                    <View
+                      key={index}
+                      style={{
+                        height: 90,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        width: "100%",
+                        paddingLeft: 20
+                      }}
+                    >
+                      <Text style={{ width: "20%" }} fontSize={'$6'} color={'$background'}> {produto.quantity}</Text>
+
+                      <View style={{ width: "80%", gap: 7 }}>
+                        <Text fontSize={'$4'} color={'$background'}><Text color={'$gray10'} >SKU: </Text>{produto.reference}</Text>
+                        <Text fontSize={'$4'} color={'$background'}  ><Text color={'$gray10'} >Descricao: </Text>  {produto.database_name}</Text>
+
+                      </View>
+                    </View>
+
+
+                  </Center>
+                </VStack>
+              </TouchableOpacity>
+
             );
           })}
         </ScrollView>
       )}
-      <Dialog onOpenChange={() => setOpenModal(false)} modal open={openModal}>
-        <Adapt when="sm" platform="touch">
-          <Sheet zIndex={200000} modal dismissOnSnapToBottom>
-            <Sheet.Frame padding="$4" gap="$4">
-              <Adapt.Contents />
-            </Sheet.Frame>
-            <Sheet.Overlay enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
-          </Sheet>
-        </Adapt>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : undefined}>
+        <Dialog onOpenChange={() => setOpenModal(false)} modal open={openModal}>
+          <Adapt when="sm" platform="touch">
+            <Sheet zIndex={200000} modal dismissOnSnapToBottom>
+              <Sheet.Frame padding="$4" gap="$4">
+                <Adapt.Contents />
+              </Sheet.Frame>
+              <Sheet.Overlay enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+            </Sheet>
+          </Adapt>
 
-        <Dialog.Portal>
-          <Dialog.Overlay
-            key="overlay"
-            opacity={0.5}
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-          />
+          <Dialog.Portal>
+            <Dialog.Overlay
+              key="overlay"
+              opacity={0.5}
+              enterStyle={{ opacity: 0 }}
+              exitStyle={{ opacity: 0 }}
+            />
 
-          <Dialog.Content
-            bordered
-            elevate
-            key="content"
-            animateOnly={["transform", "opacity"]}
-            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-            gap="$4"
-          >
-            <View style={{ justifyContent: "center", alignItems: "center" }}>
-              <Dialog.Title>Selecionar Quantidade</Dialog.Title>
-            </View>
-
-            <View style={{ justifyContent: "center", alignItems: "center" }}>
-              <Dialog.Description>
-                <Heading style={{ color: "white" }}>Produto teste 123456</Heading>
-              </Dialog.Description>
-            </View>
-            <Fieldset gap="$4">
+            <Dialog.Content
+              bordered
+              elevate
+              key="content"
+              animateOnly={["transform", "opacity"]}
+              enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+              exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+              gap="$4"
+            >
               <View style={{ justifyContent: "center", alignItems: "center" }}>
-                <H5>SKU:tete</H5>
+                <Dialog.Title textAlign='center' size={'$7'}>{produtoRef.current.database_name}</Dialog.Title>
               </View>
-            </Fieldset>
 
-            <Fieldset gap="$4">
               <View style={{ justifyContent: "center", alignItems: "center" }}>
-                <Label justifyContent="flex-end" htmlFor="quantity">
-                  Digite a quantidade
-                </Label>
-                <Input id="quantity" />
+                <Dialog.Description>
+                  <H5 style={{ color: "white" }}> </H5>
+                </Dialog.Description>
               </View>
-            </Fieldset>
+              <Fieldset gap="$4">
+                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                  <H5>SKU:{produtoRef.current.reference}</H5>
+                </View>
+              </Fieldset>
 
-            <XStack alignItems={"center"} justifyContent="center" gap="$4">
-              <Dialog.Close displayWhenAdapted asChild>
-                <Button onPress={() => setOpenModal(false)} aria-label="Close">
-                  Confirmar
-                </Button>
-              </Dialog.Close>
-            </XStack>
+              <Fieldset gap="$4">
+                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                  <Label justifyContent="flex-end" htmlFor="quantity">
+                    Digite a quantidade
+                  </Label>
+                  <Input id="quantity" />
+                </View>
+              </Fieldset>
 
-            <Unspaced>
-              <Dialog.Close asChild>
-                <Button position="absolute" top="$3" right="$3" />
-              </Dialog.Close>
-            </Unspaced>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog>
+              <XStack alignItems={"center"} justifyContent="center" gap="$4">
+                <Dialog.Close displayWhenAdapted asChild>
+                  <Button onPress={() => setOpenModal(false)} aria-label="Close">
+                    Confirmar
+                  </Button>
+                </Dialog.Close>
+              </XStack>
+
+              <Unspaced>
+                <Dialog.Close asChild>
+                  <Button position="absolute" top="$3" right="$3" />
+                </Dialog.Close>
+              </Unspaced>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog>
+      </KeyboardAvoidingView>
     </>
   );
 }
