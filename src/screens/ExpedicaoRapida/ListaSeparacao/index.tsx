e18e40d12685f7ee046402bc9e8cc6e4e156f1c6
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-02-08 14:14:39 -0300

    feat: change import

--- src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
+++ src/screens/ExpedicaoRapida/ListaSeparacao/index.tsx
@@ -1,5 +1,4 @@
 import { View, Text, Alert } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { X } from "@tamagui/lucide-icons";
 import React, { useState } from "react";
 import ArrowBack from "../../../components/ArrowBack/ArrowBack";
@@ -17,12 +16,14 @@ import {
   Input,
   Label,
   Paragraph,
+  ScrollView,
   Sheet,
   TooltipSimple,
   Unspaced,
   XStack,
 } from "tamagui";
-import { Center, ScrollView, Spinner, VStack } from "native-base";
+import { Center, Spinner, VStack } from "native-base";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function ListaSeparacao(props: any) {
   const [openModal, setOpenModal] = useState(false);
@@ -89,92 +90,7 @@ export default function ListaSeparacao(props: any) {
                     style={{ flexDirection: "row", justifyContent: "space-around", width: "100%" }}
                   >
                     <Button>Confirmar todos</Button>
-
-                    <Dialog modal>
-                      <Dialog.Trigger asChild>
-                        <TouchableOpacity>
-                          <Button> Selecionar quantidade</Button>
-                        </TouchableOpacity>
-                      </Dialog.Trigger>
-                      <Adapt when="sm" platform="touch">
-                        <Sheet animation="quick" zIndex={200000} modal dismissOnSnapToBottom>
-                          <Sheet.Frame padding="$4" gap="$4">
-                            <Adapt.Contents />
-                          </Sheet.Frame>
-                          <Sheet.Overlay
-                            animation="quick"
-                            enterStyle={{ opacity: 0 }}
-                            exitStyle={{ opacity: 0 }}
-                          />
-                        </Sheet>
-                      </Adapt>
-
-                      <Dialog.Portal>
-                        <Dialog.Overlay
-                          key="overlay"
-                          animation="quick"
-                          opacity={0.5}
-                          enterStyle={{ opacity: 0 }}
-                          exitStyle={{ opacity: 0 }}
-                        />
-
-                        <Dialog.Content
-                          bordered
-                          elevate
-                          key="content"
-                          animateOnly={["transform", "opacity"]}
-                          animation={[
-                            "quick",
-                            {
-                              opacity: {
-                                overshootClamping: true,
-                              },
-                            },
-                          ]}
-                          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-                          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-                          gap="$4"
-                        >
-                          <View style={{ justifyContent: "center", alignItems: "center" }}>
-                            <Dialog.Title>Selecionar Quantidade</Dialog.Title>
-                          </View>
-
-                          <View style={{ justifyContent: "center", alignItems: "center" }}>
-                            <Dialog.Description>
-                              <Heading style={{ color: "white" }}>Produto teste 123456</Heading>
-                            </Dialog.Description>
-                          </View>
-                          <Fieldset gap="$4">
-                            <View style={{ justifyContent: "center", alignItems: "center" }}>
-                              <H5>SKU:tete</H5>
-                            </View>
-                          </Fieldset>
-
-                          <Fieldset gap="$4">
-                            <View style={{ justifyContent: "center", alignItems: "center" }}>
-                              <Label justifyContent="flex-end" htmlFor="quantity">
-                                Digite a quantidade
-                              </Label>
-                              <Input id="quantity" />
-                            </View>
-                          </Fieldset>
-
-                          <XStack alignItems={"center"} justifyContent="center" gap="$4">
-                            <Dialog.Close displayWhenAdapted asChild>
-                              <Button onPress={() => setOpenModal(false)} aria-label="Close">
-                                Confirmar
-                              </Button>
-                            </Dialog.Close>
-                          </XStack>
-
-                          <Unspaced>
-                            <Dialog.Close asChild>
-                              <Button position="absolute" top="$3" right="$3" />
-                            </Dialog.Close>
-                          </Unspaced>
-                        </Dialog.Content>
-                      </Dialog.Portal>
-                    </Dialog>
+                    <Button onPress={() => setOpenModal(true)}> Selecionar quantidade</Button>
                   </View>
                 </Center>
               </VStack>
@@ -182,19 +98,73 @@ export default function ListaSeparacao(props: any) {
           })}
         </ScrollView>
       )}
+      <Dialog onOpenChange={() => setOpenModal(false)} modal open={openModal}>
+        <Adapt when="sm" platform="touch">
+          <Sheet zIndex={200000} modal dismissOnSnapToBottom>
+            <Sheet.Frame padding="$4" gap="$4">
+              <Adapt.Contents />
+            </Sheet.Frame>
+            <Sheet.Overlay enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+          </Sheet>
+        </Adapt>
 
-      {/* <View
-        style={{
-          paddingTop: 5,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <TouchableOpacity onPress={() => handlePrintFile(item)}>
-          <Button style={{ backgroundColor: "#002851" }}>Imprimir Arquivo</Button>
-        </TouchableOpacity>
-      </View> */}
+        <Dialog.Portal>
+          <Dialog.Overlay
+            key="overlay"
+            opacity={0.5}
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+
+          <Dialog.Content
+            bordered
+            elevate
+            key="content"
+            animateOnly={["transform", "opacity"]}
+            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+            gap="$4"
+          >
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <Dialog.Title>Selecionar Quantidade</Dialog.Title>
+            </View>
+
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <Dialog.Description>
+                <Heading style={{ color: "white" }}>Produto teste 123456</Heading>
+              </Dialog.Description>
+            </View>
+            <Fieldset gap="$4">
+              <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <H5>SKU:tete</H5>
+              </View>
+            </Fieldset>
+
+            <Fieldset gap="$4">
+              <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Label justifyContent="flex-end" htmlFor="quantity">
+                  Digite a quantidade
+                </Label>
+                <Input id="quantity" />
+              </View>
+            </Fieldset>
+
+            <XStack alignItems={"center"} justifyContent="center" gap="$4">
+              <Dialog.Close displayWhenAdapted asChild>
+                <Button onPress={() => setOpenModal(false)} aria-label="Close">
+                  Confirmar
+                </Button>
+              </Dialog.Close>
+            </XStack>
+
+            <Unspaced>
+              <Dialog.Close asChild>
+                <Button position="absolute" top="$3" right="$3" />
+              </Dialog.Close>
+            </Unspaced>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog>
     </>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,10 +15,7 @@ export function Home() {
       >
         <H4 color={"white"}>Início</H4>
       </View>
-      <View
-        backgroundColor={"$backgroundPress"}
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Image
           style={{ width: "80%", height: 200 }}
           source={require("../../../assets/liveExpedy.png")}
