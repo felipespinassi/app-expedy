d705c109030c969948f13efb355b7ad108cc4587
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-02-16 10:33:09 -0300

    refactor: remove native base import

--- src/components/ExpedicaoRapida/components/ItemsToPick/index.tsx
+++ src/components/ExpedicaoRapida/components/ItemsToPick/index.tsx
@@ -1,60 +1,50 @@
-import { KeyboardAvoidingView } from 'react-native'
-import React from 'react'
-import { Button, Card,  H4, H5, Input, Theme, View } from 'tamagui'
-import { Platform } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import { KeyboardAvoidingView } from "react-native";
+import React from "react";
+import { Button, Card, H4, H5, Input, Theme, View } from "tamagui";
+import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ItemsToPick({ produto }: any) {
-    const navigation: any = useNavigation();
-    return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+  const navigation: any = useNavigation();
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View jc={"space-around"} h={"90%"} padding={5}>
+        <View alignItems="center">
+          <H4 textAlign="center">{produto.database_name}</H4>
+        </View>
 
-        >
-            <View jc={'space-around'} h={'90%'} padding={5} >
+        <View alignItems="center">
+          <H5>SKU: {produto.reference}</H5>
+        </View>
 
-
-                <View alignItems='center' >
-
-                    <H4 textAlign='center'>{produto.database_name}</H4>
-                </View>
-
-                <View alignItems='center'  >
-
-                    <H5 >SKU: {produto.reference}</H5>
-                </View>
-
-
-                <View alignItems='center' >
-                    <Card justifyContent='center' gap={50} w={'85%'} h={'$15'} elevate size="$4" bordered>
-                        <View alignItems='center'  >
-
-                            <H5 >Quantidade necessária: {produto.quantity}</H5>
-                        </View>
-
-
-                        <View alignItems='center' justifyContent='center' >
-                            <Input borderWidth={2} keyboardType='numeric' w={'$6'} />
-
-                        </View>
-
-
-                    </Card>
-
-                </View>
-
-
-
-                <Theme inverse >
-                    <View alignItems='center'>
-
-                        <Button onPress={() => navigation.navigate('ListaSeparacao')}>Confirmar</Button>
-                    </View>
-                </Theme>
+        <View alignItems="center">
+          <Card
+            justifyContent="center"
+            gap={50}
+            w={"85%"}
+            h={"$15"}
+            elevate
+            size="$4"
+            bordered
+          >
+            <View alignItems="center">
+              <H5>Quantidade necessária: {produto.quantity}</H5>
             </View>
 
+            <View alignItems="center" justifyContent="center">
+              <Input borderWidth={2} keyboardType="numeric" w={"$6"} />
+            </View>
+          </Card>
+        </View>
 
-
-        </KeyboardAvoidingView>
-    )
+        <View alignItems="center">
+          <Button onPress={() => navigation.navigate("ListaSeparacao")}>
+            Confirmar
+          </Button>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
--- src/screens/ExpedicaoRapida/ArquivosGerados/index.tsx
+++ src/screens/ExpedicaoRapida/ArquivosGerados/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Heading } from "native-base";
 import ArrowBack from "../../../components/ArrowBack/ArrowBack";
 import { useNavigation } from "@react-navigation/native";
 import ArquivosGeradosComponent from "../../../components/ExpedicaoRapida/components/ArquivosGerados";
-import { View } from "tamagui";
+import { Heading, View } from "tamagui";
 
 export default function ArquivosGerados() {
   const navigation = useNavigation();
@@ -17,9 +16,7 @@ export default function ArquivosGerados() {
         paddingBottom={15}
       >
         <ArrowBack navigation={navigation} />
-        <Heading fontWeight={500} size={"md"} color={"white"}>
-          Arquivos Gerados
-        </Heading>
+        <Heading color={"white"}>Arquivos Gerados</Heading>
       </View>
       <ArquivosGeradosComponent />
     </>
