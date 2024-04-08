73f6830d6a8a51a3277a81f3902a5e6eb8497f26
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-04-07 23:36:35 -0300

    refactor: remove unnecessary code

--- src/screens/Mais/index.tsx
+++ src/screens/Mais/index.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 
 import React, { useState, useEffect, useContext } from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -6,8 +6,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { COMPANY_NAME } from "../../storage/storageConfig";
 import { removeCompanyName } from "../../storage/removeCompanyName";
 import { removeAccess_token } from "../../storage/removeAccess_token";
-import { Avatar, ScrollView, Text, View } from "tamagui";
+import { Avatar } from "tamagui";
 import { AuthContext } from "../../context/AuthContext";
+import { View } from "react-native";
 
 export default function Mais({ navigation }: any) {
   const [companyName, setCompanyName] = useState("");
@@ -22,90 +23,92 @@ export default function Mais({ navigation }: any) {
   useEffect(() => {
     getCompanyName();
   }, []);
+
+  const values = [
+    {
+      name: "Início",
+      icon: <Ionicons name="home-outline" size={26} />,
+      action: () => navigation.navigate("Home"),
+    },
+    {
+      name: "Pedidos",
+      icon: <Ionicons name="cart-outline" size={26} />,
+      action: () => navigation.navigate("Pedidos"),
+    },
+    {
+      name: "Expedição",
+      icon: <Ionicons name="cube-outline" size={26} />,
+      action: () => navigation.navigate("Expedição"),
+    },
+    {
+      name: "Sair",
+      icon: <Ionicons name="log-out-outline" size={26} />,
+      action: () => {
+        removeAccess_token(), removeCompanyName(), setIsLogged(false);
+      },
+    },
+  ];
   return (
     <>
-      <View height={"22%"} backgroundColor={"#002851"} flexDirection="row">
-        <View flexDirection="row" alignItems="flex-end" paddingLeft={20}>
+      <View
+        style={{
+          height: "22%",
+          backgroundColor: "#002851",
+          flexDirection: "row",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "flex-end",
+            paddingLeft: 20,
+          }}
+        >
           <View
-            flexDirection="row"
-            alignItems="center"
-            marginBottom={20}
-            maxWidth={"90%"}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 20,
+              maxWidth: "90%",
+            }}
           >
             <Avatar borderRadius={50} bg="white">
-              <Text color={"black"} textTransform="uppercase" fontSize={22}>
+              <Text style={{ textTransform: "uppercase", fontSize: 22 }}>
                 {companyName?.split("")[0]}
               </Text>
             </Avatar>
             <View>
-              <Text color={"white"} fontSize={20} marginLeft={10}>
+              <Text style={{ color: "white", fontSize: 20, marginLeft: 10 }}>
                 {companyName}
               </Text>
             </View>
           </View>
         </View>
       </View>
-      <ScrollView theme={"light"} paddingHorizontal={15}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Home")}
-          style={{ flexDirection: "row", padding: 5 }}
-        >
-          <View height={40} width={"100%"} justifyContent="center">
-            <View flexDirection="row" alignItems="center">
-              <Ionicons name="home-outline" size={26} />
-              <Text fontSize={20} paddingLeft={10}>
-                Início
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Pedidos")}
-          style={{ flexDirection: "row", padding: 5 }}
-        >
-          <View height={40} width={"100%"} justifyContent="center">
-            <View flexDirection="row" alignItems="center">
-              <Ionicons name="cart-outline" size={26} />
-
-              <Text paddingLeft={10} fontSize={20}>
-                Pedidos
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Expedicao")}
-          style={{ flexDirection: "row", padding: 5 }}
-        >
-          <View justifyContent="center" height={40} width={"100%"}>
-            <View flexDirection="row" alignItems="center">
-              <Ionicons name="cube-outline" size={26} />
-
-              <Text paddingLeft={10} fontSize={20}>
-                Expedição
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => {
-            removeAccess_token(), removeCompanyName(), setIsLogged(false);
-          }}
-          style={{ flexDirection: "row", padding: 5 }}
-        >
-          <View height={40} w={"100%"} justifyContent="center">
-            <View flexDirection="row" alignItems="center">
-              <Ionicons name="log-out-outline" size={26} />
-
-              <Text fontSize={20} paddingLeft={10}>
-                Sair
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
+      <ScrollView style={{ paddingHorizontal: 15, gap: 20 }}>
+        {values.map((element) => {
+          return (
+            <TouchableOpacity
+              key={element.name}
+              onPress={element.action}
+              style={{
+                flexDirection: "row",
+                padding: 5,
+              }}
+            >
+              <View
+                style={{ height: 40, width: "100%", justifyContent: "center" }}
+              >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  {element.icon}
+                  <Text style={{ fontSize: 20, paddingLeft: 10 }}>
+                    {element.name}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
     </>
   );
